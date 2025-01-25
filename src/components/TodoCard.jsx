732d6de8ea88bf354d98fb8af2e484ed30b7a94d
sanz19 <ra.sanz19@info.uas.edu.mx>
2025-01-24 20:09:47 -0700

    first commit

---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function TodoCard(props) {
+    const {children, handleDeleteTodo, index, handleEditTodo} = props
+  return (
+    <li className="todoItem">
+        {children}
+      <div className="actionsContainer">
+        <button onClick={()=>{handleEditTodo(index)}}><i class="fa-solid fa-marker"></i></button>
+        <button onClick={()=>{handleDeleteTodo(index)}}><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </li>
+  );
+}
